2eed4896ec40134c28f4e09c0bffc4be8b10983c
Jeff Wallace <jeff@evolvingweb.ca>
2011-04-04 14:22:01 -0400

    First commit for lunchcheck command. Still needs some tweaks.

--- base/recipes/desktop.rb
+++ base/recipes/desktop.rb
@@ -10,3 +10,12 @@
 ).each do |pkg|
   package pkg
 end
+
+%w(activexuser activezenity lunchcheck).each do |file|
+  cookbook_file "/usr/local/sbin/#{file}" do
+    owner "root"
+    group "root"
+    mode 0700
+    backup false
+  end
+end
