583875fe3cbd49335b9b6a98699a74bcc8dcef23
Jonathan Villemaire-Krajden <jonvk@evolvingweb.ca>
2011-05-06 17:51:41 -0400

    Users recipe also creates groups that users are members of and adds each user that is part of that group to the group.

--- users/recipes/default.rb
+++ users/recipes/default.rb
@@ -3,11 +3,31 @@
 # Recipe:: default
 #
 
-search :users, node[:users][:groups].map{ |g| "groups:#{g}" }.join(" OR ") do |u|
+gem_package "ruby-shadow"
+
+users = search :users, node[:users][:groups].map{ |g| "groups:#{g}" }.join(" OR ")
+
+# Get groups and members in each group
+groups = {}
+users.map do |u|
+  groupset = u[:groups].kind_of?(Array) ? u[:groups] : [ u[:groups] ]
+  groupset.each do |g|
+    if groups.include? g
+      groups[g] << u[:id]
+    else
+      groups[g] = [ u[:id] ]
+    end
+  end
+end
+
+users.each do |u|
   home_dir = "/home/#{u['id']}"
 
   user u['id'] do
     shell u['shell']
+    if not u['shadow'].nil? and not u['shadow'].empty?
+      password u['shadow']
+    end
     comment u['comment']
     supports :manage_home => true
     home home_dir
@@ -26,6 +46,12 @@ search :users, node[:users][:groups].map{ |g| "groups:#{g}" }.join(" OR ") do |u
     group u['id']
     mode 0600
     variables :ssh_keys => u['ssh_keys']
+  end
+end
+
+groups.each do |g, users|
+  group g do
+    members users
   end
 end
 
