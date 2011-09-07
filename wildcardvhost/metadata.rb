232c5d20142f2bc7b7ef6d67be347f6ea70ef094
Tavish Armstrong <tavish@evolvingweb.ca>
2011-09-07 15:50:09 -0400

    Added wildcardvhost cookbook.

---
+++ wildcardvhost/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Evolving Web Inc."
+maintainer_email "tavish@evolvingweb.ca"
+license          "All rights reserved"
+description      "Installs/Configures wildcardvhost"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
