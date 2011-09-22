ceeba82b0b54c7017d94c3b01fc5b60667c6c4a2
Tavish Armstrong <tavish@evolvingweb.ca>
2011-09-22 10:01:32 -0400

    Fixed permissions on dotfiles recipe.

--- users/recipes/dotfiles.rb
+++ users/recipes/dotfiles.rb
@@ -1,3 +1,4 @@
+
 include_recipe "users::default"
 
 users = search :users, node[:users][:groups].map{ |g| "groups:#{g}" }.join(" OR ")
@@ -8,10 +9,17 @@ users.each do |u|
     dotfiles = u['dotfiles']
     dotfiles_url = u['dotfiles']['url']
 
+    execute "change perms #{u['id']}" do
+      command "chown -R #{u['id']}:#{u['id']} #{home_dir}/dotfiles"
+      action :nothing
+    end
+
     git "dotfiles" do
       repository dotfiles_url
       destination "#{home_dir}/dotfiles"
+      reference "master"
       action :sync
+      notifies :run, resources(:execute => "change perms #{u['id']}")
     end
   end
 end
