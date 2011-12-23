ff1018afd9eb394c8edca01826a9a97915f18bc9
Tavish Armstrong <tavish@evolvingweb.ca>
2011-12-23 16:17:18 -0500

    Add phpmyadmin recipe.

---
+++ phpmyadmin/metadata.rb
@@ -0,0 +1,9 @@
+maintainer       "MoJo Code"
+maintainer_email "yabawock@gmail.com"
+license          "Apache 2.0"
+description      "Installs/Configures phpmyadmin"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version          "0.0.1"
+depends		 "apache2"
+depends		 "mysql"
+depends		 "php"
---
+++ phpmyadmin/recipes/default.rb
@@ -0,0 +1,30 @@
+#
+# Cookbook Name:: phpmyadmin
+# Recipe:: default
+#
+# Copyright 2010, MoJo Code
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+include_recipe "apache2::default"
+include_recipe "mysql::server"
+include_recipe "php"
+
+package "phpmyadmin" do
+  action :upgrade
+end
+
+link "/etc/apache2/conf.d/phpmyadmin" do
+  to "/etc/phpmyadmin/apache.conf"
+end
